6b27ee293bc97885c57f04f9020cf0726715a6e3
ArkanJeyy <arkanfarrasabyan@gmail.com>
2025-10-02 14:56:56 +0700

    Modifikasi Percobaan 1

--- ifCekKRS05.java
+++ ifCekKRS05.java
@@ -13,7 +13,7 @@ public class ifCekKRS05 {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
         }else{
-            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
+            System.out.println("Mohon maaf Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
 
     }
