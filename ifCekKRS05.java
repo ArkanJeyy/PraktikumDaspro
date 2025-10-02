15233248f825e24b7dd0ffec3039a49adf4a7130
ArkanJeyy <arkanfarrasabyan@gmail.com>
2025-10-02 14:55:55 +0700

    Modifikasi Tugas 1

--- ifCekKRS05.java
+++ ifCekKRS05.java
@@ -8,12 +8,13 @@ public class ifCekKRS05 {
         System.out.print("Apakah UKT sudah lunas? (iya/tidak): ");
         boolean uktLunas = sc.nextBoolean();
 
-        System.out.println(uktLunas ? "Bisa ikut ujian" : "Tidak bisa ikut ujian" );
-
-        // if (uktLunas) {
-        //     System.out.println("Pembayaran UKT terverifikasi");
-        //     System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
-
+        // System.out.println(uktLunas ? "Bisa ikut ujian" : "Tidak bisa ikut ujian" );
+        if (uktLunas) {
+            System.out.println("Pembayaran UKT terverifikasi");
+            System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+        }else{
+            System.out.println("Registrasi ditolak. Silahkan lunasi UKT terlebih dahulu");
         }
 
     }
+}
